5b976437738ed2caa80b31b5779f5c2a27527418
Junes <55406230+Sykes-ops@users.noreply.github.com>
2022-03-16 12:31:47 +0100

    Workshop 15.03.2022

---
+++ Unity/YAMM - Workshop/Assets/Scripts/Workshop.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Workshop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string wort2 = "Hallo";
+        
+        // if(zahl == 4) 
+        // {
+        //     Debug.Log("Super");
+        //     Debug.Log("Toll");
+        // }
+        // else if(zahl == 3) 
+        // {
+        //     Debug.Log("supi");
+        // }
+        // else 
+        // {
+        //     Debug.Log("Schade");
+        //     Debug.Log("Schade");
+        // }
+
+        // switch(wort2)
+        // {
+        //     case "Hallo":
+        //         Debug.Log("Zahl ist 1");
+        //         break;
+        //     case "hallo":
+        //         Debug.Log("Zahl ist 2");
+        //         break;
+        //     case "hallO": 
+        //         Debug.Log("Zahl ist 3");
+        //         break;
+        //     default:
+        //         Debug.Log("Default");
+        //         break;
+        // }
+
+        int[] array = new int[5] {1, 3, 5, 98, 3};
+        // for(int i = 0; i < array.Length; i++)
+        // {
+        //     Debug.Log(array[i]);
+        // }
+
+        // foreach(int variable in array)
+        // {
+        //     Debug.Log(variable);
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
